048e979674c2a3e46150d33156b6987641aa5e7d
Benjamin Andoh <benoo.andoh@gmail.com>
2023-01-13 15:15:20 +0000

    this is a microservice app
    it is intented to recieve messages from a django application

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from crypt import methods
+from dataclasses import dataclass
+from itertools import product
+from urllib import request
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 from flask_migrate import Migrate
@@ -15,12 +19,17 @@ from flask_cors import CORS
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
@@ -28,9 +37,14 @@ class ProductUser(db.Model):
 
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
-@app.route("/")
+@app.route('/api/products')
 def index():
-    return "<p>Hello, World! Here to stay forever</p>"
+    return jsonify(Product.query.all())
+
+@app.route('/api/product/<int:id>/like', methods=['POST'])
+def like(id):
+    req = request.get('http://localhost:8000/api/user')
+    return jsonify(req.json)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
